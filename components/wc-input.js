754f58b1677fcf4d4d2f82c22e823f4266738bf2
Maggie Wachs <maggie@filamentgroup.com>
2022-06-22 14:48:53 -0400

    updated accessibility

--- components/wc-input.js
+++ components/wc-input.js
@@ -2,57 +2,61 @@ class SimpleInput extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({
-      mode: "open",
+      mode: 'open',
       delegatesFocus: true,
     });
 
-    const label = document.createElement("label");
-    const labelText = document.createElement("span");
-    const errorMssg = document.createElement("p");
-    labelText.classList.add("label-text");
-    labelText.textContent = this.getAttribute("label");
-    errorMssg.classList.add("error-message");
+    const label = document.createElement('label');
+    const labelText = document.createElement('span');
+    const errorMssg = document.createElement('p');
+    labelText.classList.add('label-text');
+    labelText.textContent = this.getAttribute('label');
+    errorMssg.setAttribute('id', 'input-error');
+    errorMssg.classList.add('error-message');
 
-    const input = document.createElement("input");
+    const input = document.createElement('input');
+    input.setAttribute('aria-describedby', 'input-error');
 
     // set default value, if any
-    if (this.hasAttribute("value")) {
-      let val = this.getAttribute("value");
+    if (this.hasAttribute('value')) {
+      let val = this.getAttribute('value');
       if (val) input.value = val;
     }
 
     // check for required attr
-    if (this.hasAttribute("required")) {
+    if (this.hasAttribute('required')) {
       // set corresponding input property
       input.required = true;
       // by default, the field is invalid until filled in
-      this.setAttribute("invalid", true);
-      // add a label class
-      labelText.classList.add("required");
+      this.setAttribute('invalid', true);
+      // add indicator, styles
+      labelText.innerHTML = labelText.innerHTML +=
+        '<sup aria-hidden="true">*</sup>';
+      labelText.classList.add('required');
     }
 
     this.isInvalid = () => {
-      input.classList.add("invalid");
-      this.setAttribute("invalid", true);
-      errorMssg.textContent = "Please enter a value.";
+      this.setAttribute('invalid', true);
+      errorMssg.textContent = 'Please enter a value.';
+      input.classList.add('invalid');
     };
 
     this.isValid = () => {
-      input.classList.remove("invalid");
-      this.removeAttribute("invalid");
-      errorMssg.textContent = "";
+      this.removeAttribute('invalid');
+      errorMssg.textContent = '';
+      input.classList.remove('invalid');
     };
 
-    input.addEventListener("change", (e) => {
+    input.addEventListener('change', (e) => {
       // sync value with attr
-      this.setAttribute("value", input.value);
+      this.setAttribute('value', input.value);
 
       // dispatch change event that bubbles from the input
-      let changeEv = new Event("change", { bubbles: true });
+      let changeEv = new Event('change', { bubbles: true });
       this.dispatchEvent(changeEv);
     });
 
-    input.addEventListener("blur", (e) => {
+    input.addEventListener('blur', (e) => {
       // run client-side validation
       if (input.validity.valueMissing) {
         this.isInvalid();
@@ -61,7 +65,7 @@ class SimpleInput extends HTMLElement {
       }
     });
 
-    const style = document.createElement("style");
+    const style = document.createElement('style');
     style.textContent = `
       label {
         display: flex;
@@ -70,9 +74,12 @@ class SimpleInput extends HTMLElement {
       .label-text {
         flex: 0 0 8rem;
       }
-      .label-text.required:after {
-        content: "*";
-        color: #b50000;
+      .label-text sup {
+        color: red;
+        font-size: 1.2em;
+        vertical-align: unset;
+        margin-top: -1em;
+        display: inline-block;
       }
       .error-message {
         font-size: .9em;
@@ -91,7 +98,7 @@ class SimpleInput extends HTMLElement {
       }
     `;
 
-    this.classList.add("field");
+    this.classList.add('field');
 
     label.appendChild(labelText);
     label.appendChild(input);
@@ -101,4 +108,4 @@ class SimpleInput extends HTMLElement {
   }
 }
 
-customElements.define("simple-input", SimpleInput);
+customElements.define('simple-input', SimpleInput);
